a01491960630cddc84e369e78afd086c05918439
Brent Bauman <brentbauman86@gmail.com>
2018-02-20 19:54:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist
 };
 
-function removeFromPlaylist( Jim Croce, I got a name){
+function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName;
   return playlist
 };
