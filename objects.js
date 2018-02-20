15bffd37e53f195afb4f60eacdf3a3a2e14e3428
Brent Bauman <brentbauman86@gmail.com>
2018-02-20 19:44:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = { 'Jim Croce': 'I got a name', 'TEB': 'wounded', 'DC Talk': 'supe
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
-  return updatePlaylist
+  return playlist
 };
