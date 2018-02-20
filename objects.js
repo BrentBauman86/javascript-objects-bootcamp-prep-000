34da73701ea73abcb1305129900696064a47f402
Brent Bauman <brentbauman86@gmail.com>
2018-02-20 19:31:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = { 'Jim Croce': 'I got a name', 'TEB': 'wounded', 'DC Talk': 'supernova'};
 
 function updatePlaylist('playlist', 'Jim Croce', 'I got a name'){
+  playlist['Jim Croce'] = 'I got a name'
   return updatePlaylist
 };
